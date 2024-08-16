dd1b4731859e81b3a4a43737091334b22253eba5
Trane <Trane_67@hotmail.de>
2024-08-17 00:52:46 +0200

    addtask.html added

---
+++ javascript/addtask.js
@@ -0,0 +1,17 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+function filterFunction() {
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+  const div = document.getElementById("myDropdown");
+  const a = div.getElementsByTagName("a");
+  for (let i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
