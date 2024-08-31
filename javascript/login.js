22dc998df6775032e6a8bccd8e1b5e5dcc3d18ee
Denis-Dudda <denis.d@hotmail.de>
2024-08-31 13:10:16 +0200

    save 31.08.24

--- javascript/login.js
+++ javascript/login.js
@@ -2,7 +2,7 @@ let BASE_URL = "https://join-318-default-rtdb.europe-west1.firebasedatabase.app/
 
 function start(){
   startLogInAnimation();
-  includeHTML();
+  
 }
 
 function startLogInAnimation() {
